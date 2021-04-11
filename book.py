f1a1008175fa405d496eea8c9f871c2099c90791
kooser6 <48822466+kooser6@users.noreply.github.com>
2021-04-11 17:23:43 -0400

    Create book.py

---
+++ book.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env pypy
+# -*- coding: utf-8 -*-
+
+import random
+
+class Book:
+    def __init__(self,path=""):
+        self.book={}
+        if path!="": self.import_book(path)
+        
+    def import_book(self,path):
+        with open(path) as fp:
+            line=fp.readline()
+            while line:
+                line=line.strip()
+                line=line.split(" ")
+                move=line[-1]
+                line=line[:-1]
+                line=' '.join(line)
+                book_move=self.book.get(line)
+                if book_move: self.book[line].append(move)
+                else: self.book[line]=[move]
+                line=fp.readline()
+        fp.close()
+ 
+    def get_move(self,line):
+        book_move=self.book.get(line)
+        if book_move:
+            if len(book_move)==1: return book_move[0]
+            else: return book_move[random.randint(0,len(book_move)-1)]
+        return ()
